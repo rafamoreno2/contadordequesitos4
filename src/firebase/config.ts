7bb3f23c39aac874aa5c262e47d56c019e260579
afon Afon <afonafon554@gmail.com>
2025-10-03 11:41:08 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6042595309-ca7ae",
+  "appId": "1:592067305567:web:92bf222479749a0a5abbea",
+  "apiKey": "AIzaSyCi49IGlL12cDz6ldSP_vsfagyPQSXlc5o",
+  "authDomain": "studio-6042595309-ca7ae.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "592067305567"
+};
