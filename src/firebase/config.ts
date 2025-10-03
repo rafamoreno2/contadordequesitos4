9d58f2ecd8f9a203836cd1c99f18eed473ca7f2a
afon Afon <afonafon554@gmail.com>
2025-10-03 11:55:52 +0000

    Y ahora si paso el código a un repo de github y subo el repo a vercel se

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
-  "projectId": "studio-6042595309-ca7ae",
-  "appId": "1:592067305567:web:92bf222479749a0a5abbea",
-  "apiKey": "AIzaSyCi49IGlL12cDz6ldSP_vsfagyPQSXlc5o",
-  "authDomain": "studio-6042595309-ca7ae.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "592067305567"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
