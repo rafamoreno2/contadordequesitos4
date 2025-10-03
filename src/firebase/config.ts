6c96d233689ff2a2f187bc23da4bf414b56e0b54
afon Afon <afonafon554@gmail.com>
2025-10-03 13:03:11 +0000

    mejor dejalo como antes no importa que sea poco seguro ya que asi es mas

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyCi49IGlL12cDz6ldSP_vsfagyPQSXlc5o",
+  authDomain: "studio-6042595309-ca7ae.firebaseapp.com",
+  projectId: "studio-6042595309-ca7ae",
+  storageBucket: "studio-6042595309-ca7ae.appspot.com",
+  messagingSenderId: "592067305567",
+  appId: "1:592067305567:web:92bf222479749a0a5abbea",
+  measurementId: "G-D68S6GHEJG"
 };
