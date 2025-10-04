85d3b2302f470301aab090f125e5e3471351f9ae
afon Afon <afonafon554@gmail.com>
2025-10-04 11:32:30 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -34,6 +34,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'em-content.zobj.net',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
